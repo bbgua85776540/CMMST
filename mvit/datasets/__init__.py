1760e37d4ab0872982e9282b481cc8655edb656b
bbgua85776540 <369487529@qq.com>
2024-08-02 17:35:36 +0800

    Add files via upload

---
+++ mvit/datasets/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+
+from .build import build_dataset, DATASET_REGISTRY  # noqa
+from .imagenet import Imagenet  # noqa
+from .vrdataset import Vrdataset  # noqa
+from .evaldataset import Evaldataset  # noqa
+# from .utils import get_act_tiles2  # noqa
+# from .utils import get_act_tiles3  # noqa
