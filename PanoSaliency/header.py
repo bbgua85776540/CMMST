59c4dfe18923adc15afd47c4de99c6c75c38c344
bbgua85776540 <369487529@qq.com>
2024-12-23 12:44:00 +0800

    Delete PanoSaliency directory

--- PanoSaliency/header.py
+++
@@ -1,6 +0,0 @@
-# dirpath1 = u'./data/head-orientation/dataset1'
-# dirpath2 = u'./data/head-orientation/dataset2/Experiment_1'
-dirpath1 = u'E:/dataset/360-degree_video_head_movement_dataset/dataset'
-dirpath2 = u'E:/dataset/vr_dataset/Experiment_1'
-ext1 = '.txt'
-ext2 = '.csv'
